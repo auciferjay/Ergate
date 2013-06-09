6071a3d49aa02f2733af1cdf42662b57c62cc15a
auciferjay <auciferjay@gmail.com>
2013-06-09 15:55:53 +0800

    update

--- scripts/background.js
+++ scripts/background.js
@@ -19,21 +19,21 @@ chrome.contextMenus.create({
 	"title" : "Pure Text", 
 	"parentId" : "selected",
 	"contexts" : ["all"],
-	"onclick" : getClickHandler()
+	"onclick" : getClickHandler(1)
 });
 
 chrome.contextMenus.create({
 	"title" : "HTML Text", 
 	"parentId" : "selected",
 	"contexts" : ["all"],
-	"onclick" : getClickHandler()
+	"onclick" : getClickHandler(2)
 });
 
 chrome.contextMenus.create({
 	"title" : "URL", 
 	"parentId" : "main",
 	"contexts" : ["all"],
-	"onclick" : getClickHandler()
+	"onclick" : getClickHandler(3)
 });
 /*
 chrome.contextMenus.create({
@@ -43,12 +43,13 @@ chrome.contextMenus.create({
 	"onclick" : getClickHandler()
 });
 */
+
 /**
  * Returns a handler which will open a new window when activated.
  */
-function getClickHandler() {
+function getClickHandler(type) {
 	return function(info, tab) {
-		alert(info.menuItemId+":"+info.parentMenuItemId+":"+info.mediaType+":"+info.linkUrl+":"+info.srcUrl+":"+info.pageUrl+":"+info.frameUrl+":"+info.selectionText);
-		chrome.tabs.sendRequest(tab.id, {name: "COPY_MESSAGE"});
+		//alert(info.menuItemId+":"+info.parentMenuItemId+":"+info.mediaType+":"+info.linkUrl+":"+info.srcUrl+":"+info.pageUrl+":"+info.frameUrl+":"+info.selectionText);
+		chrome.tabs.sendRequest(tab.id, {name: "COPY_MESSAGE", type: type});
 	};
 };
--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,86 @@
+// communication with the background page
 chrome.extension.onRequest.addListener(
 	function(request, sender, sendResponse) {
-		
+		if( request.name == "COPY_MESSAGE" ) {
+			switch( request.type ){
+				case 1:
+					contextMenuElementXPath.innerText;
+					break;
+				case 2:
+					contextMenuElementXPath.innerHTML;
+					break;
+				case 3:
+					document.location;
+					break;
+			}
+			//var contextMenuElement = getElementsByXPath(document, contextMenuElementXPath)[0];
+			
+			
+		}
 	}
 );
+
+var contextMenuElementXPath = null;
+
+var onContextMenu = function(event)
+{
+    contextMenuElementXPath = event.target;//getElementXPath(event.target);
+};
+
+var loadListeners = function()
+{
+    window.addEventListener("contextmenu", onContextMenu);
+};
+
+var getElementXPath = function(element)
+{
+    if (element && element.id)
+        return '//*[@id="' + element.id + '"]';
+    else
+        return this.getElementTreeXPath(element);
+};
+
+var getElementTreeXPath = function(element)
+{
+    var paths = [];
+
+    for (; element && element.nodeType == 1; element = element.parentNode)
+    {
+        var index = 0;
+        var nodeName = element.nodeName;
+
+        for (var sibling = element.previousSibling; sibling; sibling = sibling.previousSibling)
+        {
+            if (sibling.nodeType != 1) continue;
+
+            if (sibling.nodeName == nodeName)
+                ++index;
+        }
+
+        var tagName = element.nodeName.toLowerCase();
+        var pathIndex = (index ? "[" + (index+1) + "]" : "");
+        paths.splice(0, 0, tagName + pathIndex);
+    }
+
+    return paths.length ? "/" + paths.join("/") : null;
+};
+
+var getElementsByXPath = function(doc, xpath)
+{
+    var nodes = [];
+
+    try {
+        var result = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null);
+        for (var item = result.iterateNext(); item; item = result.iterateNext())
+            nodes.push(item);
+    }
+    catch (exc)
+    {
+        // Invalid xpath expressions make their way here sometimes.  If that happens,
+        // we still want to return an empty set without an exception.
+    }
+
+    return nodes;
+};
+
+loadListeners();
