680695a11e58da8c6b1ae2b3521c5e434ec43772
auciferjay <auciferjay@gmail.com>
2013-06-14 19:32:33 +0800

    update

--- nodejs/requestHandlers.js
+++ nodejs/requestHandlers.js
@@ -1,18 +1,112 @@
 var querystring = require('querystring');
+var mysql = require('mysql');//npm install mysql
+
+var config = {
+				host: 'localhost',
+				port: 3306,
+				//socketPath:,
+				user: 'root';
+				password: 'root';
+				database: 'test',
+				insecureAuth: false,
+				supportBigNumbers: false,
+				bigNumberStrings: false,
+				//debug:,
+				stringifyObjects: false,
+				timezone: 'local',
+				flags: '',
+				//queryFormat:,
+				//pool:,
+				multipleStatements: false,
+				typeCast: true,
+				//charset:
+			 };
+			 
+var db;
+
+function onConnect() {
+	db = mysql.createConnection(config);
+	db.connect(handleError);
+	db.on('error', handleError);
+}
+
+function handleError (err) {
+	if (err) {
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			connect();
+		} else {
+			console.error(err.stack || err);
+		}
+	}
+}
 
 function login(data, res ) {
-	console.log(data['name']);
 	console.log("Request handler 'login' was called.");
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write(querystring.stringify(data));
-	res.end();
+	
+	db.query("SELECT * FROM [user] WHERE username='"+data['name']+"' and password='"+data['pswd']+"';", function(error, results) {
+		if(err) {
+			console.log("ClientReady Error: " + error.message);
+			db.end();
+			
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write(querystring.stringify({code:0,message:'db error'}));
+			res.end();
+			return;
+		}
+		
+		console.log('Effected: ' + results.affectedRows + ' row.');
+		console.log('Id inserted: ' + results.insertId);
+		
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write(querystring.stringify({code:1,message:''}));
+		res.end();
+	});
 }
 
 function logout(data, res) {
 	console.log("Request handler 'logout' was called.");
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write(querystring.stringify(data));
-	res.end();
+	
+	db.query("SELECT * FROM [user] WHERE username='"+data['name']+"' and password='"+data['pswd']+"';", function(error, results) {
+		if(err) {
+			console.log("ClientReady Error: " + error.message);
+			db.end();
+			
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write(querystring.stringify({code:0,message:'db error'}));
+			res.end();
+			return;
+		}
+		
+		console.log('Effected: ' + results.affectedRows + ' row.');
+		console.log('Id inserted: ' + results.insertId);
+		
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write(querystring.stringify({code:1,message:''}));
+		res.end();
+	});
+}
+
+function send(data, res) {
+	console.log("Request handler 'send' was called.");
+	
+	db.query("SELECT * FROM [user] WHERE username='"+data['name']+"' and password='"+data['pswd']+"';", function(error, results) {
+		if(err) {
+			console.log("ClientReady Error: " + error.message);
+			db.end();
+			
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write(querystring.stringify({code:0,message:'db error'}));
+			res.end();
+			return;
+		}
+		
+		console.log('Effected: ' + results.affectedRows + ' row.');
+		console.log('Id inserted: ' + results.insertId);
+		
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write(querystring.stringify({code:1,message:''}));
+		res.end();
+	});
 }
 
 exports.login = login;
--- nodejs/server.js
+++ nodejs/server.js
@@ -2,8 +2,12 @@ var http = require('http');
 var url = require("url");
 var querystring = require('querystring');
 
+var sessionFactory = require('./session');
+
 function onStart(route, handler) {
 	http.createServer(function(req, res) {
+		sessionFactory.startSession(req, res, handler);
+		
 		console.log("Request for " + pathname + " received.");
 		var pathname = url.parse(req.url).pathname;
 		var query = url.parse(req.url).query;
@@ -23,6 +27,12 @@ function onStart(route, handler) {
 		});
 	}).listen(80, '127.0.0.1');
 	console.log("Server has started.");
+}
+
+var handler = function(req, res) {
+	var session = this;
+	session.set('banana', '你个巴啦~');
+	res.end(session.get('banana'));
 }
 
 exports.start = onStart;
